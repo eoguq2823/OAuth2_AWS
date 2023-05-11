a032f6208fc7b2ad1f46f7b93e2349329ceccdd5
eoguq2823 <eoguq9227@naver.com>
2023-05-11 13:41:04 +0900

    feat - generateAccessToken메소드 생성 (로그인 시 AccessToken발급후 각각의 로그인에 분배 )

--- oauth2-back/src/main/java/com/study/oauth2/security/jwt/JwtTokenProvider.java
+++ oauth2-back/src/main/java/com/study/oauth2/security/jwt/JwtTokenProvider.java
@@ -6,9 +6,12 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+
+import com.study.oauth2.security.PrincipalUser;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -22,6 +25,42 @@ public class JwtTokenProvider {
 	
 	public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
 		key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+	}
+	
+	public String generateAccessToken(Authentication authentication) {
+		
+		String email = null;
+
+		if(authentication.getPrincipal().getClass() == UserDetails.class) {
+			//일반로그인일때 (PrincipalUser
+			PrincipalUser principalUser = (PrincipalUser) authentication.getPrincipal();
+			email = principalUser.getEmail();
+		} else {
+			//oauth로그인일때 (OAuth2User)
+			OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+			email = oAuth2User.getAttribute("email");
+		}
+		//권한설정
+		if(authentication.getAuthorities() == null) {
+			throw new RuntimeException("등록된 권한이 없습니다.");
+		}
+		StringBuilder roles = new StringBuilder();
+		
+		authentication.getAuthorities().forEach(authority -> {
+			roles.append(authority.getAuthority() + ",");
+		});
+		roles.delete(roles.length() - 1, roles.length());
+		
+		Date toKenExpiresDate = new Date(new Date().getTime() + (1000 * 60 * 60 * 24));
+		
+		return Jwts.builder()
+				.setSubject("AccessToken")
+				.claim("email", email)
+				.claim("auth", roles)
+				.setExpiration(toKenExpiresDate)
+				.signWith(key, SignatureAlgorithm.HS256)
+				.compact();
+				
 	}
 	
 	public String generateOAuth2RegisterToken(Authentication authentication) {
